31552b8f047515dd3ddd0894f541360def5de9e5
Adam <adam.deere@hotmail.com>
2023-03-14 20:26:40 +0000

    Multi texturing finished

--- TowerRemaster/Game.cs
+++ TowerRemaster/Game.cs
@@ -10,7 +10,23 @@ namespace TowerRemaster
     {
         int VertexBufferObject; 
         int VertexArrayObject;
-        Shader shader;
+        int ElementBufferObject;
+
+        private Shader shader;
+        // For documentation on this, check Texture.cs.
+        private Texture _texture;
+        private Texture _textureTwo;
+        private readonly float[] vertices = {
+            //Position          Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left
+        };
+       private readonly uint[] indices = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+        };
         public Game(int width, int height, string title)
             : base(GameWindowSettings.Default, new NativeWindowSettings()
             { Size = (width, height), Title = title })
@@ -23,12 +39,6 @@ namespace TowerRemaster
             shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-            float[] vertices = {
-                -0.5f, -0.5f, 0.0f, //Bottom-left vertex
-                0.5f, -0.5f, 0.0f, //Bottom-right vertex
-                0.0f,  0.5f, 0.0f  //Top vertex
-            };
-
             VertexBufferObject = GL.GenBuffer();
             VertexArrayObject = GL.GenVertexArray();
 
@@ -37,8 +47,24 @@ namespace TowerRemaster
             GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
             // 3. then set our vertex attributes pointers
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+            int texCoordLocation = shader.GetAttribLocation("aTexCoord");
+            GL.EnableVertexAttribArray(texCoordLocation);
+            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+
             GL.EnableVertexAttribArray(0);
+
+            _texture = Texture.LoadFromFile("Resources/container.png");
+            _textureTwo = Texture.LoadFromFile("Resources/awesomeface.png");
+            shader.Use();
+            shader.SetInt("texture1", 0);
+            shader.SetInt("texture2", 1);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
@@ -58,9 +84,14 @@ namespace TowerRemaster
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            
+            _texture.Use(TextureUnit.Texture0);
+            _textureTwo.Use(TextureUnit.Texture1);
+
             shader.Use();
+
             GL.BindVertexArray(VertexArrayObject);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
 
             SwapBuffers();
         }
