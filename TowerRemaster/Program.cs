b4e6468146bed014be39d9b2793ed61513d0b944
Adam <adam.deere@hotmail.com>
2023-03-14 19:45:50 +0000

    Rendered first Triangle

---
+++ TowerRemaster/Program.cs
@@ -0,0 +1,11 @@
+﻿using TowerRemaster;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+        using Game game = new Game(800, 600, "LearnOpenTK");
+        game.Run();
+    }
+}
