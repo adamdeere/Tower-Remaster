7778f703959f90869643737abe40d30e7c828ae6
Adam <adam.deere@hotmail.com>
2023-03-14 21:19:19 +0000

    Abstracted models and model loading  into separate classes

--- TowerRemaster/Game.cs
+++ TowerRemaster/Game.cs
@@ -10,10 +10,7 @@ namespace TowerRemaster
 {
     internal class Game : GameWindow
     {
-        private int VertexBufferObject;
-        private int VertexArrayObject;
-        private int ElementBufferObject;
-
+        private Model _model;
         private Shader shader;
 
         // For documentation on this, check Texture.cs.
@@ -54,25 +51,7 @@ namespace TowerRemaster
             shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-            VertexBufferObject = GL.GenBuffer();
-            VertexArrayObject = GL.GenVertexArray();
-
-            GL.BindVertexArray(VertexArrayObject);
-            // 2. copy our vertices array in a buffer for OpenGL to use
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-            // 3. then set our vertex attributes pointers
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
-
-            int texCoordLocation = shader.GetAttribLocation("aTexCoord");
-            GL.EnableVertexAttribArray(texCoordLocation);
-            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
-
-            ElementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
-
-            GL.EnableVertexAttribArray(0);
+            _model = ModelLoader.LoadModel(vertices, indices);
 
             _texture = TextureLoader.LoadFromFile("Resources/container.png");
             _textureTwo = TextureLoader.LoadFromFile("Resources/awesomeface.png");
@@ -175,8 +154,7 @@ namespace TowerRemaster
             Matrix4 mvp = model * _camera.GetViewMatrix() * _camera.GetProjectionMatrix();
             shader.SetMatrix4("mvp", mvp);
 
-            GL.BindVertexArray(VertexArrayObject);
-            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            _model.DrawModel();
 
             SwapBuffers();
         }
@@ -192,8 +170,7 @@ namespace TowerRemaster
         {
             base.OnUnload();
 
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            GL.DeleteBuffer(VertexBufferObject);
+            _model.DisposeModel();
         }
     }
 }
---
+++ TowerRemaster/Utility/ModelLoader.cs
@@ -0,0 +1,33 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace TowerRemaster.Utility
+{
+    internal static class ModelLoader
+    {
+        public static Model LoadModel(float[] vertices, uint[] indices)
+        {
+            int VertexArrayObject = GL.GenVertexArray();
+            int VertexBufferObject = GL.GenBuffer();
+
+            GL.BindVertexArray(VertexArrayObject);
+            // 2. copy our vertices array in a buffer for OpenGL to use
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+           
+            GL.EnableVertexAttribArray(1);
+            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+
+            int ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            GL.EnableVertexAttribArray(0);
+
+            return new Model(VertexArrayObject, indices.Length);
+        }
+    }
+}
